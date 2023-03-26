75eb1db57043e8d8e290d28a21c9b582f666e50f
Ehan2022 <ibtisamehan@gmail.com>
2023-03-26 15:46:42 +0600

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,64 @@
+$('.banner_main').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5500,
+  arrows: false,
+  dots: false,
+  pauseOnHover: false
+});
+  new VenoBox({
+    selector: ".my-video-links"
+});
+new VenoBox({
+    selector: ".my-image-links1"
+});
+
+$('.slide_main').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  dots:false,
+  nextArrow:".prev",
+  prevArrow:".next",
+  centerMode: true,
+  centerPadding: '0px',
+});
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+    $('#scroll-top').fadeIn();
+  } else {
+    $('#scroll-top').fadeOut();
+  }
+});
+
+$('#scroll-top').click(function () {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 1000);
+});
+
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }
+    ]
+  });
+});
